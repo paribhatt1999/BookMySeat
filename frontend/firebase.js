9577904e96dc92608679ae453fed61bf358e850b
Pari Bhattacharya <2023btechaidspari15664@poornima.edu.in>
2026-09-22 16:41:14 +0530

    Use provided Firebase web configuration

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,6 +1,39 @@
+// Firebase Web SDK configuration and services
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-analytics.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, RecaptchaVerifier, signInWithPhoneNumber, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
-const firebaseConfig={apiKey:"AIzaSyCHEiAIStYdfnOhy0uBjOSF9vuxSDT1vfc",authDomain:"bookmyseat-8d477.firebaseapp.com",projectId:"bookmyseat-8d477",storageBucket:"bookmyseat-8d477.firebasestorage.app",messagingSenderId:"998275535843",appId:"1:998275535843:web:53f139adf29fd1c61fc6d6",measurementId:"G-Q9NGK9ZDKJ"};
-const app=initializeApp(firebaseConfig);const analytics=getAnalytics(app);const auth=getAuth(app);const googleProvider=new GoogleAuthProvider();
-export {app,analytics,auth,googleProvider,signInWithPopup,RecaptchaVerifier,signInWithPhoneNumber,onAuthStateChanged,signOut};
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHEiAIStYdfnOhy0uBjOSF9vuxSDT1vfc",
+  authDomain: "bookmyseat-8d477.firebaseapp.com",
+  projectId: "bookmyseat-8d477",
+  storageBucket: "bookmyseat-8d477.firebasestorage.app",
+  messagingSenderId: "998275535843",
+  appId: "1:998275535843:web:53f139adf29fd1c61fc6d6",
+  measurementId: "G-Q9NGK9ZDKJ",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export {
+  app,
+  analytics,
+  auth,
+  googleProvider,
+  signInWithPopup,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  onAuthStateChanged,
+  signOut,
+};
